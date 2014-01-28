bc19a903592629a412a67e0a978a4b877d3b8448
Derek Stegelman <dstegelman@gmail.com>
2014-01-28 13:07:24 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 LONG_DESC = """\
 A python wrapper to the USPS api, currently only supports address validation
 """
